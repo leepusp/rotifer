6ada60a02244d8b8763ad1dc5ec2fcc902a87dcc
castelli <castellivinicius07@gmail.com>
2026-09-26 01:59:54 -0300

    fix: :bug: unmarked documentation on webapi init file

--- lib/rotifer/db/uniprot/webapi/__init__.py
+++ lib/rotifer/db/uniprot/webapi/__init__.py
@@ -5,28 +5,6 @@ Query UniProt's REST web services.
 mapping workflow: submit a batch of accessions, poll until the job
 finishes and download the results.
 
-Note
-----
-The ``local_database_path`` configuration default below is not used
-anywhere in this package; see ``docs/OPEN_QUESTIONS.md``.
-"""
-
-import os
-import rotifer
-from rotifer import GlobalConfig
-from rotifer.core.functions import loadConfig
-logger = rotifer.logging.getLogger(__name__)
-
-Extra API parameters
---------------------
-UniProt's API accepts many options these classes know nothing about.
-Any keyword a cursor does not recognise is passed straight through as a
-query parameter, so they all remain available:
-
->>> fc = webapi.FastaCursor(includeIsoform=True)                # doctest: +SKIP
->>> sc = webapi.SearchCursor(database='uniprotkb',              # doctest: +SKIP
-...                          fields='accession,id,length')
-
 Cursors
 -------
 :class:`SequenceCursor`
@@ -42,6 +20,16 @@ Cursors
 :class:`MappingCursor`
     Identifier translation, in either direction, read off the
     cross-references an entry carries.
+
+Extra API parameters
+--------------------
+UniProt's API accepts many options these classes know nothing about.
+Any keyword a cursor does not recognise is passed straight through as a
+query parameter, so they all remain available:
+
+>>> fc = webapi.FastaCursor(includeIsoform=True)                # doctest: +SKIP
+>>> sc = webapi.SearchCursor(database='uniprotkb',              # doctest: +SKIP
+...                          fields='accession,id,length')
 
 See Also
 --------
